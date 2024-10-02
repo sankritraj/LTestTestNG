677e75a25536ddfca9973bc34483dae04079efcd
ADYANT SINGH <sankritraj@gmail.com>
2024-10-02 16:44:27 +0530

    Removing unused file

--- src/test/java/utilities/Waits.java
+++ src/test/java/utilities/Waits.java
@@ -4,13 +4,10 @@ import java.time.Duration;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import testBase.BaseClass;
 
 public class Waits {
 
